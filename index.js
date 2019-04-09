28a7228fea2238414fc5a5bf0385e26b2041a792
Jason Beedle <jason_beedle@hotmail.com>
2019-04-09 22:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function getFirstSelector(selector) {
-  return document.querySelector(selector);
+ return  document.querySelector(selector);
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested .target');
+ return document.getElementById("nested").querySelector(".target");
 }
 
 function increaseRankBy(n) {
-  var ranks = document.querySelectorAll('ul.ranked-list li');
-  for (var i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
-  }
+ var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+ for(let i=0;i<lis.length;i++) {
+   lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+ }
 }
 
 function deepestChild() {
-  var grandNode = document.querySelector('#grand-node');
-  var deepestNode = grandNode.children[0];
-  for (var i = 0; !deepestNode.children[i]; i) {
-    deepestNode = deepestNode.children[0]
-  }
-  return deepestNode;
+ var lis=document.getElementById("grand-node").querySelectorAll("div");
+ var test;
+ for(let i=0;i<lis.length-1;i++) {
+   test=lis[i].querySelector("div");
+ }
+return test;
 }
